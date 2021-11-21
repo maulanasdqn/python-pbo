b0cca4e7df69c9eb35ce66ee47ab9f523bb88f03
Maulana Sodiqin <sodiqincahayana1@gmail.com>
2021-11-22 01:57:04 +0700

    Maulana Sodiqin | Improvement Feature Beli

--- apps/Minuman.py
+++ apps/Minuman.py
@@ -1,13 +1,17 @@
 from apps.Product import Product
 
 class Minuman(Product):
+
+    #Abstarction (Constructor)
     def __init__(self,name, qty, price, variant):
         super().__init__(name, qty, price)
         self.__variant = variant
 
+    #Encapsulation (Setter)
     def setVariant(self, variant):
         self.__variant = variant
 
+    #Encapsulation (Getter)
     def infoProduct(self):
         return f"Nama : {self.getName()}\nHarga : Rp.{self.getPrice()}\nVariant : {self.__variant}"
 
