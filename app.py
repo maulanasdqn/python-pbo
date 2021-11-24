85168118e692c69ce0009c0787229006e26810a8
Maulana Sodiqin <sodiqincahayana1@gmail.com>
2021-11-24 16:58:58 +0700

    Maulana Sodiqin | Improvement Buying Logic

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from apps.Makanan import Makanan
+from os import system
 from apps.Minuman import Minuman
 from apps.Beli import Beli
 
@@ -14,26 +15,85 @@ minuman = [
 
 pembeli = []
 
-def Menu():
+keranjang = []
+
+def Pembeli():
+    print("1.Beli\n2.Lihat Pesanan\n3.Bayar\n4.Keluar")
+    pilih = int(input("Silahkan Pilih Menu : "))
+
+    if pilih == 1:
+        buyFood()
+        Pembeli()
+        system.clear()
+
+    elif pilih == 2:
+        print (f"Anda membeli : {keranjang[0].getName()}")
+        h = input ("Enter untuk kembali")
+        Pembeli()
+        system.clear()
+
+    elif pilih == 3:
+        pass
+
+    elif pilih == 4:
+        auth()
+
+
+def auth():
+    peran = str(input("Silahkan pilih peran anda : "))
+    if peran == 1:
+        print("Anda masuk sebagai Penjual")
+    elif peran == 2:
+        print("Anda masuk sebagai Pembeli")
+
+def buyFood():
     i = 0
     y = "y"
     while y == "y":
 
+        warning = "Mohon hanya input angka saja"
+
         i =+ 1 
         nama = str(input("Masukan Nama Anda : "))
-        uang = int(input("Masukan Jumlah Uang Anda : "))
 
-        for i,x in enumerate(makanan):
+        try:
+            uang = int(input("Masukan Jumlah Uang Anda : "))
+
+        except:
+            print(warning)
+            uang = int(input("Masukan Jumlah Uang Anda : "))
+
+        for i, x in enumerate(makanan):
             print (f"{i+1}.{x.getName()} Rp.{x.getPrice()}")
 
-        pilih = int(input("Silahkan Pilih Menu : "))
-        jumlah = int(input("Masukan Jumlah Pesanan : "))
-        level = int(input("Masukan Level kepedasan : "))
+        try:
+            pilih = int(input("Silahkan Pilih Menu (Input hanya angka saja) : "))
+        except:
+            print(warning)
+            pilih = int(input("Silahkan Pilih Menu (Input hanya angka saja) : "))
+        
+        opt = pilih - 1
+        menu = makanan[opt]
+        keranjang.append(menu)
+        
+        try:
+            jumlah = int(input("Masukan Jumlah Pesanan : "))
+        except:
+            print(warning)
+            jumlah = int(input("Masukan Jumlah Pesanan : "))
+        
+        print("1.Tidak Pedas\n2.Pedas Lumayan\n3.Sangat Pedas\n4.Extra Pedas")
 
-        if pilih == 1:
-            menu = makanan[0]
-        elif pilih == 2:
-            menu = makanan[1]
+        try:
+            level = int(input("Masukan Level Kepedasan : "))
+            while level < 1 or level > 4:
+                print("Mohon Masukan angka dari 1 sampai 4 saja!")
+                level = int(input("Masukan Level Kepedasan : "))
+        except:
+            level = int(input("Masukan Level Kepedasan : "))
+            while level < 1 or level > 4:
+                print("Mohon Masukan angka dari 1 sampai 4 saja!")
+                level = int(input("Masukan Level Kepedasan : "))
 
         i = Beli(nama, uang)
         i.buyFood(menu, jumlah, level)
@@ -41,7 +101,4 @@ def Menu():
         pembeli.append(i)
         y = input("Masuk : ")
 
-    for i,x in enumerate(pembeli):
-        print (f"{i+1}.{x.infoBuyerFood()}")
-
-Menu()
+Pembeli()
